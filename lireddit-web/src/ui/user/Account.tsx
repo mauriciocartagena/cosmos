e2830d4ef0882daa770ad2477dbf68d32fc59267
Mauricio Cartagena <mc973220@gmail.com>
2021-05-02 17:21:55 -0400

    Se arreglo el cargado de estilos de account

--- lireddit-web/src/ui/user/Account.tsx
+++ lireddit-web/src/ui/user/Account.tsx
@@ -9,44 +9,62 @@ import { Button } from "../Button";
 import { ProfileHeaderWrapper } from "../ProfileHeaderWrapper";
 import { SingleUser } from "../UserAvatar/SingleUser";
 import { useIsAuth } from "../../modules/auth/useIsAuth";
+import { useState } from "react";
 
-export interface AccountProps {}
-const Account: React.FC<AccountProps> = () => {
+const Account: React.FC<{}> = () => {
   useIsAuth();
+
+  const [data, setData] = useState(false);
+  setTimeout(() => {
+    setData(true);
+  }, 1000);
+
   return (
     <>
       <HeaderController embed={{}} title="Account" />
 
-      <DefaultDesktopLayout>
-        <MiddlePanel>
-          <ProfileHeaderWrapper coverUrl={"https://source.unsplash.com/random"}>
-            <div className="flex mr-4 ">
-              <SingleUser
-                isOnline={true}
-                className="absolute flex-none -top-5.5 rounded-full shadow-outlineLg bg-primary-900"
-                src={"https://avatars.githubusercontent.com/u/51917913?v=4"}
-              />
-            </div>
-            <div className="flex flex-col w-3/6 font-sans">
-              <h4 className="text-primary-100 font-bold truncate">Mauricio</h4>
-              <div className="flex flex-row items-center">
-                <p
-                  className="text-primary-300 mr-2"
-                  data-testid="profile-info-username"
-                >{`@Mauricio`}</p>
+      {data ? (
+        <DefaultDesktopLayout>
+          <MiddlePanel>
+            <ProfileHeaderWrapper
+              coverUrl={"https://source.unsplash.com/random"}
+            >
+              <div className="flex mr-4 ">
+                <SingleUser
+                  isOnline={true}
+                  className="absolute flex-none -top-5.5 rounded-full shadow-outlineLg bg-primary-900"
+                  src={"https://avatars.githubusercontent.com/u/51917913?v=4"}
+                />
               </div>
-            </div>
+              <div className="flex flex-col w-3/6 font-sans">
+                <h4 className="text-primary-100 font-bold truncate">
+                  Mauricio
+                </h4>
+                <div className="flex flex-row items-center">
+                  <p
+                    className="text-primary-300 mr-2"
+                    data-testid="profile-info-username"
+                  >{`@Mauricio`}</p>
+                </div>
+              </div>
 
-            <div className="w-3/6 ">
-              <div className="flex flex-row justify-end content-end gap-2">
-                <Button size="small" color="secondary" icon={<SolidCompass />}>
-                  Editar cuenta
-                </Button>
+              <div className="w-3/6 ">
+                <div className="flex flex-row justify-end content-end gap-2">
+                  <Button
+                    size="small"
+                    color="secondary"
+                    icon={<SolidCompass />}
+                  >
+                    Editar cuenta
+                  </Button>
+                </div>
               </div>
-            </div>
-          </ProfileHeaderWrapper>
-        </MiddlePanel>
-      </DefaultDesktopLayout>
+            </ProfileHeaderWrapper>
+          </MiddlePanel>
+        </DefaultDesktopLayout>
+      ) : (
+        <div>Loading ...</div>
+      )}
     </>
   );
 };
